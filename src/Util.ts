564862966b1084ed4af24ff9f2e851e7ae8931ab
rhangai <hangai.renan@gmail.com>
2020-03-18 10:16:35 -0300

    chore: Udated formatting

--- src/Util.ts
+++ src/Util.ts
@@ -3,16 +3,13 @@ export type Prototype<T> = T & { constructor: Class<T> };
 
 export const SYMBOL_VALIDATOR_DECORATOR = Symbol('validator-decorator');
 
-type DeepReadonly<T> = T extends (infer R)[]
-	? DeepReadonlyArray<R>
-	: T extends Function
-	? T
-	: T extends {}
-	? DeepReadonlyObject<T>
-	: T;
-
+// prettier-ignore
+type DeepReadonly<T> = 
+	T extends (infer R)[] ? DeepReadonlyArray<R> : 
+	T extends Function ? T : 
+	T extends {} ? DeepReadonlyObject<T> : 
+	T;
 interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
-
 type DeepReadonlyObject<T> = {
 	readonly [P in keyof T]: DeepReadonly<T[P]>;
 };
