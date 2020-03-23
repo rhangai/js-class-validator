3af280fde54c75a2bdea52cc901d5c7a47aedced
rhangai <hangai.renan@gmail.com>
2020-03-23 11:41:41 -0300

    fix: Date types on ValidateInput and Readonly

--- src/Util.ts
+++ src/Util.ts
@@ -8,8 +8,10 @@ export const VALIDATOR_SYMBOL_INPUT = Symbol('validator-input-type');
 type DeepReadonly<T> = 
 	T extends (infer R)[] ? DeepReadonlyArray<R> : 
 	T extends Function ? T : 
+	T extends Date ? T : 
 	T extends {} ? DeepReadonlyObject<T> : 
 	T;
+
 interface DeepReadonlyArray<T> extends ReadonlyArray<DeepReadonly<T>> {}
 type DeepReadonlyObject<T> = {
 	readonly [P in keyof T]: DeepReadonly<T[P]>;
@@ -22,15 +24,16 @@ interface ValidateInputObjectConversible<InputType> {
 }
 
 // prettier-ignore
-type UnvalidatedField<T> = 
+type ValidateInputValue<T> = 
 	T extends ValidateInputObjectConversible<infer U> ? U : 
 	T extends Array<infer U> ? Array<ValidateInputObject<U>> : 
+	T extends Date ? (string | Date) : 
 	T extends Object ? ValidateInputObject<T> : 
 	T;
 
 // prettier-ignore
 type ValidateInputObject<T> = {
-	[K in keyof T]?: UnvalidatedField<T[K]>;
+	[K in keyof T]?: ValidateInputValue<T[K]>;
 };
 
 export type ValidateInput<T> = ValidateInputObject<T>;
